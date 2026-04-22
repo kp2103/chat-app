8b1a0f2a406811b41af5ba2547547789f4706c64
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-04-22 15:43:16 +0530

    remove server related code from ui branch

--- server/config/db_connect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(process.env.DB_CONNECT,).then(()=>{
-    console.log("DB connected sucessfully")
-}).catch((err)=>{
-    console.log("Error in connecting db:",err)
-    process.exit(1);
-})
