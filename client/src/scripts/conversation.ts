8355433d6aa27ed675dcb123656dfc63eff3f20c
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-04-23 17:22:44 +0530

    fix issue of showing empty chat panel when user opens ssite

--- client/src/scripts/conversation.ts
+++ client/src/scripts/conversation.ts
@@ -1,3 +1,5 @@
+import { createMessageCard } from './message.js';
+import { socket } from './script.js';
 import { phoneNumber } from './setVariable.js';
 import { getPersonAvatar, getPersonName } from './utils.js';
 
@@ -9,6 +11,91 @@ interface Participant {
 }
 
 const convList = document.getElementById('conv-list') as HTMLUListElement;
+const chatArea = document.getElementById('chat-panel') as HTMLDivElement;
+const chatBody = document.getElementById('chat-body') as HTMLDivElement;
+const inputMessage = document.getElementById('msg-input') as HTMLInputElement;
+const sendBtn = document.getElementById('btn-send') as HTMLButtonElement;
+const chatHeaderImageEle = document.querySelector(
+	'.chat-header__info img',
+) as HTMLImageElement;
+const chatTitle = document.getElementById('chat-title') as HTMLPreElement;
+
+let currentConversationId: null | string = null;
+
+chatArea.style.display = 'none';
+
+convList.addEventListener('click', (e) => {
+	const target = e.target as HTMLElement | null;
+	const convItemEle = target?.closest('.conv-item') as HTMLLIElement | null;
+
+	if (!convItemEle) return;
+
+	chatBody.innerHTML = '';
+
+	currentConversationId = convItemEle.dataset.conversationId!;
+
+	socket.emit('join-room', currentConversationId);
+
+	chatArea.style.display = 'flex';
+
+	loadMessages(currentConversationId);
+
+	const avatarUrl = convItemEle.querySelector('img')!.src;
+	const name = convItemEle.querySelector('.conv-item__name')!.textContent;
+
+	updateChatHeader({ avatarUrl, name });
+});
+
+async function loadMessages(roomId: string) {
+	const res = await fetch(`http://localhost:4000/api/v1/messages/${roomId}`, {
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json',
+		},
+	});
+
+	const resObj = await res.json();
+	const messages = await resObj.messages;
+
+	for (const message of messages) {
+		createMessageCard({
+			message: message.content.text,
+			timeStamp: message.createdAt,
+			senderMobileNumber: message.senderMobileNumber,
+			userMobileNumber: phoneNumber,
+		});
+	}
+}
+
+async function sendMessage(e: PointerEvent) {
+	if (!currentConversationId) return;
+
+	const messageObj = {
+		message: inputMessage.value,
+		type: 'Text',
+		roomId: currentConversationId,
+		senderMobileNumber: phoneNumber,
+	};
+
+	socket.emit(
+		'send-message',
+		messageObj,
+		(obj: { status: number; isSuccess: boolean; message: string }) => {
+			if (obj.isSuccess) {
+				createMessageCard({
+					message: inputMessage.value,
+					timeStamp: new Date().toString(),
+					userMobileNumber: phoneNumber,
+					senderMobileNumber: phoneNumber,
+				});
+
+				inputMessage.value = '';
+			}
+		},
+	);
+}
+
+sendBtn.addEventListener('click', (e) => sendMessage(e));
 
 function createAvatarElement({ participants }: { participants: Participant[] }) {
 	const convItemAvatarEle = document.createElement('div');
@@ -109,6 +196,13 @@ function createConversationCard({
 	listEle.appendChild(convItemBodyEle);
 
 	convList.appendChild(listEle);
+}
+
+function updateChatHeader({ avatarUrl, name }: { avatarUrl: string; name: string }) {
+	chatHeaderImageEle.src = avatarUrl;
+	chatHeaderImageEle.alt = name;
+
+	chatTitle.textContent = name;
 }
 
 export { type Participant, createConversationCard };
--- client/src/scripts/script.ts
+++ client/src/scripts/script.ts
@@ -3,16 +3,12 @@ import { createMessageCard } from './message.js';
 import { phoneNumber } from './setVariable.js';
 import { createUserProfile } from './user.js';
 
-const sendBtn = document.getElementById('btn-send') as HTMLButtonElement;
-const inputMessage = document.getElementById('msg-input') as HTMLInputElement;
-
 declare const io: any;
 
 const socket = io(`http://localhost:4000`, {
 	transports: ['websocket'],
 });
 
-socket.emit('join-room', '50d90482-2b55-4dc3-ba74-35b4e7a81117');
 
 socket.on('receive-message', (msg: string) => {
 	if (msg) {
@@ -23,54 +19,6 @@ socket.on('receive-message', (msg: string) => {
 		});
 	}
 });
-
-sendBtn.addEventListener('click', (e) => sendMessage(e));
-
-async function sendMessage(e: PointerEvent) {
-	const messageObj = {
-		message: inputMessage.value,
-		type: 'Text',
-		roomId: '50d90482-2b55-4dc3-ba74-35b4e7a81117',
-		senderMobileNumber: phoneNumber,
-	};
-
-	socket.emit(
-		'send-message',
-		messageObj,
-		(obj: { status: number; isSuccess: boolean; message: string }) => {
-			if (obj.isSuccess) {
-				createMessageCard({
-					message: inputMessage.value,
-					timeStamp: new Date().toString(),
-					userMobileNumber: phoneNumber,
-					senderMobileNumber: phoneNumber,
-				});
-			}
-		},
-	);
-}
-
-async function loadMessages(roomId: string) {
-	const res = await fetch(`http://localhost:4000/api/v1/messages/${roomId}`, {
-		method: 'GET',
-		headers: {
-			'Content-type': 'application/json',
-		},
-	});
-
-	const resObj = await res.json();
-	const messages = await resObj.messages;
-
-	for (const message of messages) {
-		createMessageCard({
-			message: message.content.text,
-			timeStamp: message.createdAt,
-			senderMobileNumber: message.senderMobileNumber,
-			userMobileNumber: phoneNumber,
-		});
-	}
-}
-loadMessages('50d90482-2b55-4dc3-ba74-35b4e7a81117');
 
 async function loadConversation(mobileNumber: string) {
 	const res = await fetch(
@@ -115,3 +63,5 @@ async function loadUser(mobileNumber: string) {
 	});
 }
 loadUser(phoneNumber);
+
+export { socket };
