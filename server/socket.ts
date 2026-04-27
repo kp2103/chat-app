66fc4c029aa210e5ba42a24af200e8eb39cb8fc1
krish-patel-simform <krish.patel@simformsolutions.com>
2026-04-27 16:57:42 +0530

    fix:resolve bug of sedning receive message before storing in DB

--- server/socket.ts
+++ server/socket.ts
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 import { createMessage } from "./services/createMessage.service.ts";
 
 interface ServerToClientEvents{
-    "receive-message": (message:string)=> void
+    "receive-message": (data:{message:string,senderMobileNumber:string})=> void
 }
 
 interface ClientToServerEvents{
@@ -59,10 +59,8 @@ export function initSocket(httpServer: HTTPServer)
                     message:res.message,
                     isSuccess:res.isSuccess
                 })
+                socket.to(roomId).emit('receive-message',{message,senderMobileNumber})
             })
-
-            socket.to(roomId).emit('receive-message',message)
-
         })
     })
 }
